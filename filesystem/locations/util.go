d0f8c29fd0d5f142c9451f6c4cb22025901c06a1
Aurelio Buonomo <48835871+auribuo@users.noreply.github.com>
2024-10-20 18:08:33 +0200

    Fixed bug in Definition() function

--- filesystem/locations/util.go
+++ filesystem/locations/util.go
@@ -6,6 +6,7 @@ import (
 	"github.com/buonotti/apisense/filesystem/locations/directories"
 )
 
+// Returns the full path of a definition given it's filename without extension
 func Definition(name string) string {
-	return filepath.FromSlash(directories.DefinitionsDirectory() + name + "apisensedef.yml")
+	return filepath.FromSlash(directories.DefinitionsDirectory() + "/" + name + "apisensedef.yml")
 }
